5cdb207119792d9b54ab20c6df85bb360d125bc6
Luís Peixoto <lmpeixoto@gmail.com>
2025-11-01 09:36:34 +0000

    Initial commit

---
+++ src/main/java/com/lmpeixoto/reservation/restaurant_reservation_system/RestaurantReservationSystemApplication.java
@@ -0,0 +1,14 @@
+package com.lmpeixoto.reservation.restaurant_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationSystemApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+		SpringApplication.run(RestaurantReservationSystemApplication.class, args);
+	}
+
+}
